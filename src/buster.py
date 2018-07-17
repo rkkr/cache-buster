d3a0e70b8ff93160e0114f80a1f476d714e334d8
Raimondas Rimkus <narimantukas@gmail.com>
2018-07-17 06:52:00 +0300

    First commit

---
+++ src/buster.py
@@ -0,0 +1,21 @@
+import webapp2
+from google.appengine.api import urlfetch
+
+class MainPage(webapp2.RequestHandler):
+    def get(self):
+        if 'redirecturl' not in self.request.GET.keys():
+            self.response.status = '400 Bad Request'
+            self.response.write('Invalid url')
+            return
+
+        url = self.request.GET['redirecturl']
+        remote = urlfetch.fetch(url=url, method=urlfetch.GET, headers=self.request.headers)
+        #Cache-Control: no-cache header added by default
+        self.response.status_int = remote.status_code
+        for name, value in remote.headers.iteritems():
+            self.response.headers[name] = value
+        self.response.write(remote.content)
+
+app = webapp2.WSGIApplication([
+    ('/', MainPage),
+], debug=True)
