c20eb5d7962b61c46f107ba0862134083ece4f30
Raimondas Rimkus <narimantukas@gmail.com>
2018-07-18 21:34:11 +0300

    Add UI

--- src/buster.py
+++ src/buster.py
@@ -3,12 +3,12 @@ from google.appengine.api import urlfetch
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        if 'redirecturl' not in self.request.GET.keys():
+        if 'url' not in self.request.GET.keys():
             self.response.status = '400 Bad Request'
             self.response.write('Invalid url')
             return
 
-        url = self.request.GET['redirecturl']
+        url = self.request.GET['url']
         remote = urlfetch.fetch(url=url, method=urlfetch.GET, headers=self.request.headers)
         #Cache-Control: no-cache header added by default
         self.response.status_int = remote.status_code
